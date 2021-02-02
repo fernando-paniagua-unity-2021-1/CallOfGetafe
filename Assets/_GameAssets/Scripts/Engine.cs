c61de611a96dbfa9a882e664a344cf62860b28f1
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-02-02 21:03:50 +0100

    Sesion 2021-02-02
    Agua
    Viento
    Árboles
    Piedras y otros elementos 3D
    Motocicleta y coches

---
+++ Assets/_GameAssets/Scripts/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    [Range(0,0.1f)]
+    public float amplitude;
+    [Range(0, 1)]
+    public float frecuency;
+
+    private int direction = 1;
+
+    private void Start()
+    {
+        InvokeRepeating("Mover", 0, frecuency);
+    }
+    private void Mover()
+    {
+        transform.Translate(Vector3.up * amplitude * direction);
+        direction *= -1;
+    }
+}
