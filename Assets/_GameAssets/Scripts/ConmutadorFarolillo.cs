b5868932f1d8aa0baaa86959caea46a8f66954f0
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-02-04 21:03:17 +0100

    Sesion 2021-02-04
    Escena de interior
    Iluminación: point light, area, spot light y directional light.
    Implementación de linterna de mano con spot light y script de encendido y apagado (tecla L).

---
+++ Assets/_GameAssets/Scripts/ConmutadorFarolillo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConmutadorFarolillo : MonoBehaviour
+{
+    public Light luz1;
+    public Light luz2;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name=="Claudia")
+        {
+            luz1.enabled = true;
+            luz2.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Claudia")
+        {
+            luz1.enabled = false;
+            luz2.enabled = false;
+        }
+    }
+}
