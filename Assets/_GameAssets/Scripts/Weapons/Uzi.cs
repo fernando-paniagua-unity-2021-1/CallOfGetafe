f204d5eed8d139ef1e5615fc6f669d9f7a804b1f
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-02-08 21:06:36 +0100

    Sesion 2021-02-08
    Disparo de enemigo estático
    Control de armas
    Disparo de armas con proyectil

---
+++ Assets/_GameAssets/Scripts/Weapons/Uzi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uzi : Weapon
+{
+    public GameObject prefabProyectil;
+    public float fuerzaDisparo;
+    public GameObject puntoSpawn;
+    public override void Disparar()
+    {
+        PlaySonidoDisparo();
+        GameObject proyectil = Instantiate(prefabProyectil, puntoSpawn.transform.position, puntoSpawn.transform.rotation);
+        proyectil.GetComponent<Rigidbody>().AddForce(puntoSpawn.transform.forward * fuerzaDisparo);
+    }
+
+    public override void Recargar()
+    {
+        PlaySonidoRecarga();
+    }
+}
