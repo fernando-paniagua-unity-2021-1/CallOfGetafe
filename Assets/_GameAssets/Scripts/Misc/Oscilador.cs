160ada34b498481b6e15081c363e5616f1a65631
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-02-09 21:07:26 +0100

    Sesion 2021-02-09
    Configuración de materiales.
    Mesh collider en edificios.
    Scripts de rotación y oscilado.

---
+++ Assets/_GameAssets/Scripts/Misc/Oscilador.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilador : MonoBehaviour
+{
+    private float yActual;
+    private float yInicial;
+    private float yFinal;
+    private float porcentaje = 0;
+
+    [Range(0,10)]
+    public float distancia;
+    [Range(0,50)]
+    public float speed;
+
+    private void Start()
+    {
+        yInicial = transform.position.y;
+        yFinal = yInicial + distancia;
+    }
+
+    void Update()
+    {
+        porcentaje = (Mathf.Sin(Time.time * speed) + 1) / 2;
+        yActual = Mathf.Lerp(yInicial, yFinal, porcentaje);
+        transform.position = new Vector3(transform.position.x, yActual, transform.position.z);
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Misc/Rotador.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotador : MonoBehaviour
+{
+    [Range(-200,200)]
+    public float speed;
+    private void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
