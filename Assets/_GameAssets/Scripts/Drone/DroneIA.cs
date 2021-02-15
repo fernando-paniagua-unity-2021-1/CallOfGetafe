47473064e3b5e4411265deff6cfb903cd18db239
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-02-15 21:10:57 +0100

    Sesion 2021-02-15
    ProBuilder
    Navmesh

---
+++ Assets/_GameAssets/Scripts/Drone/DroneIA.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DroneIA : MonoBehaviour
+{
+    public NavMeshAgent agente;
+    public Transform[] puntosPatrullaje;
+    public bool navegacionSecuencial;
+
+    private int puntoActual = 0;
+    private int puntoSiguiente;
+    private void Start()
+    {
+        agente.SetDestination(puntosPatrullaje[puntoActual].position);
+    }
+    private void Update()
+    {
+        if (agente.remainingDistance <= 0)
+        {
+            if (navegacionSecuencial)
+            {
+                puntoSiguiente=puntoActual+1;
+            } else
+            {
+                do
+                {
+                    //Si sólo hay un punto de patrullaje, sale del bucle
+                    if (puntosPatrullaje.Length==1) break;
+                    puntoSiguiente = Random.Range(0, puntosPatrullaje.Length);
+                } while (puntoSiguiente == puntoActual);
+            }
+            puntoActual = puntoSiguiente;
+            if (puntoActual==puntosPatrullaje.Length) {
+                puntoActual = 0;
+            }
+            agente.SetDestination(puntosPatrullaje[puntoActual].position);
+        }
+    }
+}
