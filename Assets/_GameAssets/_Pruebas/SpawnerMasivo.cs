143006a1eb279e152a66f40a8dac01d41d1ec4a9
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-02-19 21:06:52 +0100

    Sesion 2021-02-19
    Ejemplos física

---
+++ Assets/_GameAssets/_Pruebas/SpawnerMasivo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerMasivo : MonoBehaviour
+{
+    public float fuerza;
+    public float numeroObjetos;
+    public GameObject prefab;
+
+    private void Start()
+    {
+        SpawnObjectsRandomDirection();
+    }
+
+    private void SpawnObjectsRandomDirection()
+    {
+        for (int i = 0; i < numeroObjetos; i++)
+        {
+            GameObject cubo = Instantiate(prefab, transform.position, transform.rotation);
+            cubo.GetComponent<Rigidbody>().AddForce(
+                new Vector3(
+                    Random.Range(-fuerza, fuerza),
+                    fuerza,
+                    Random.Range(-fuerza, fuerza)));
+        }
+    }
+}
